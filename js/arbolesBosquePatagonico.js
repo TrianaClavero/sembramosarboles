722ba7ec776fda785ffbbde4b7e6e1fdb6e6a34c
TrianaClavero <110431642+TrianaClavero@users.noreply.github.com>
2022-10-22 20:10:26 -0300

    agregado de JSON

---
+++ js/arbolesBosquePatagonico.js
@@ -0,0 +1,18 @@
+let cad=`<div class="container">
+       `
+for(arbol of dataBP){
+    cad+=`<div class="tarjeta">
+            <img src="${arbol.image}" alt="${arbol.name}">
+            <div class="cuerpo">
+                <h4>${arbol.name}</h4>
+                <p>Especie:${arbol.species}</p>                
+                <p>Genero:${arbol.gender}</p>
+            </div>
+        </div>
+    `
+}
+cad+=`</div>
+      `       
+console.log(cad)
+document.getElementById("fotos").innerHTML=cad
+
---
+++ js/datos.js
@@ -0,0 +1,73 @@
+var dataBP= [
+    {
+    "id": 1,
+    "name": "LAURA",
+    "gender": "Schinus",
+    "species": "patagonicus",
+
+    "image": "./imagenes/Laura_Schinus_patagonicus.jpg",
+    },
+    {
+    "id": 2,
+    "name": "ÑIRE",
+    "gender": "Nothofagus",
+    "species": "antarctica",
+
+    "image": "./imagenes/Nire_Nothofagus_antarctica.jpg",
+    },
+    {
+    "id": 3,
+    "name": "MAITÉN",
+    "gender": "Maytenus",
+    "species": "boaria",
+
+    "image": "./imagenes/Maiten_Maytenus_boaria.jpg",
+    
+    },
+    {
+    "id": 4,
+    "name": "CIPRÉS de la cordillera",
+    "gender": "Austrocedrus",
+    "species": "A. chilensis",
+
+    "image": "./imagenes/Cipres_de_la_cordillera_Austrocedrus_chilensis.JPG",
+    }
+]
+
+var dataSM= [
+    {
+    "id": 5,
+    "name": "ÁRBOL DE ARTIGAS",
+    "gender": "Peltophorum",
+    "species": "dubium",
+
+    "image": "./imagenes/ARBOL_DE_ARTIGAS_Peltophorum_dubium.jpg",
+    },
+    {
+    "id": 6,
+    "name": "AGICO BLANCO",
+    "gender": "Albizia",
+    "species": "niopoides",
+
+    "image": "./imagenes/ANGICO_BLANCO_Albizia_niopoides.jpg",
+    },
+    {
+    "id": 7,
+    "name": "ESPINA CORONA",
+    "gender": "Gleditsia",
+    "species": "amorphoides",
+
+    "image": "./imagenes/ESPINA_CORONA_Gleditsia_amorphoides.jpg",
+    
+    },
+    {
+    "id": 8,
+    "name": "PETERIBI",
+    "gender": "Cordia",
+    "species": "trichotoma",
+
+    "image": "./imagenes/PETERIBI_Cordia_trichotoma.JPG",
+    }
+]
+/* 
+*/
